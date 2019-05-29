afccdeda7641b9208d5646687dd339d4ce21af36
Andrey Posudevsky | Colours <a.posudevsky@colours.nl>
2019-05-29 14:49:23 +0200

    feat(404 and thank you pages)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'gatsby';
 import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <section id="main" className="wrapper">
+      <div className="inner">
+          <h1 className="major">Page not found</h1>
+          <p>You have hit a route that doesn't exist. Try going to <Link to="/">Homepage</Link></p>
+      </div>
+    </section>
   </Layout>
-)
+);
 
 export default NotFoundPage
--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -3,12 +3,10 @@ import Layout from '../../components/Layout'
 
 export default () => (
   <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
-        </div>
+    <section id="main" className="wrapper">
+      <div className="inner">
+          <h1 className="major">Thank you for your interest!</h1>
+          <p>I will get back to you shortly.</p>
       </div>
     </section>
   </Layout>
