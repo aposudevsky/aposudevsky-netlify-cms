9388678ddd026e30b417aa8d16771f5d9e4d4ac7
Andrey Posudevsky <aposudevsky@gmail.com>
2023-07-27 17:05:54 +0200

    google analytics migrated

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,16 +93,47 @@ module.exports = {
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-offline',
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //       trackingId: "UA-141218511-1",
+    //       // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //       head: false,
+    //       // Setting this parameter is optional
+    //       anonymize: true,
+    //       // Setting this parameter is also optional
+    //       respectDNT: true,
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-          trackingId: "UA-141218511-1",
-          // Defines where to place the tracking script - `true` in the head and `false` in the body
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "387777545", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          // optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
           head: false,
-          // Setting this parameter is optional
-          anonymize: true,
           // Setting this parameter is also optional
           respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/admin"],
+          // Defaults to https://www.googletagmanager.com
+          // origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
       },
     },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
